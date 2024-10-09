c5e492cc0b5c97440c41b45ff2fea40750ada18e
Admin <jainmintu557@gmail.com>
2024-10-10 04:48:02 +0530

    add all 8 oct files

---
+++ src/oct/ex_08102024_VariablesandDatatypes/Lab005.java
@@ -0,0 +1,25 @@
+package oct.ex_08102024_VariablesandDatatypes;
+
+public class Lab005
+{
+    public static void main(String[] args) {
+      //  int age =30;
+        // data type = int - whole number
+        // age = variable_name
+        // = assignment operator
+        // 30-> literal/ variable name
+
+      // int age1=35;
+     //  int $=123;
+     //  int shashank =123;
+     // int class=23;  invalid
+     // int public=23; invalid
+
+        int _1=256;
+        int _2=546;
+        int a_b=123;
+        System.out.println(_2);
+
+
+    }
+}
---
+++ src/oct/ex_08102024_VariablesandDatatypes/Lab006.java
@@ -0,0 +1,15 @@
+package oct.ex_08102024_VariablesandDatatypes;
+
+public class Lab006
+{
+    public static void main(String[] args) {
+        //boolean 123 =true;
+        boolean _123=true;
+       // boolean _ =true;
+        boolean shashank_automation =true;
+        System.out.println(shashank_automation);
+        boolean shashank123 =false;
+        boolean _shashank=false;
+        System.out.println(_shashank);
+    }
+}
