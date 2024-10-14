c93ab3779e00cd3786f7b37ac69f6f6b42d9b8f0
Admin <jainmintu557@gmail.com>
2024-10-15 01:19:55 +0530

    recent files

---
+++ src/oct/ex_10102024/Lab009.java
@@ -0,0 +1,17 @@
+package oct.ex_10102024;
+
+public class Lab009
+{
+    public static void main(String[] args) {
+       //ESCAPE CHAR
+        char new_line='\n';
+        char tab_line='\t';
+        char back_space='\b';// delete last word of first letter
+        char car_r= '\r'; //delete the previous word
+        System.out.println("shashank"+new_line+"jain");
+        System.out.println("shashank"+tab_line+"jain");
+        System.out.println("shashank"+back_space+"jain");
+        System.out.println("shashank"+car_r+"jain");
+
+    }
+}
