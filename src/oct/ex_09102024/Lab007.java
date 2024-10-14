9929150bd6aa51f624b4961d2a12dc95036e0b7a
Admin <jainmintu557@gmail.com>
2024-10-14 22:08:25 +0530

    recent files

---
+++ src/oct/ex_09102024/Lab007.java
@@ -0,0 +1,13 @@
+package oct.ex_09102024;
+
+public class Lab007
+{
+    public static void main(String[] args) {
+      /*  System.out.println("hello world");
+        System.out.print("shashank");
+        System.out.print("shashank");
+        System.out.print("shashank");*/
+        int age= 12;
+        System.out.printf("your age is %s",age);
+            }
+}
