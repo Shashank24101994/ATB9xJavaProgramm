360cd2d6b13525b8f68cda8635a43423a576f136
Admin <jainmintu557@gmail.com>
2024-10-21 21:58:09 +0530

    recent files all

--- src/DailyTask/Task3_15oct.java
+++ src/DailyTask/Task3_15oct.java
@@ -5,6 +5,6 @@ public class Task3_15oct
     public static void main(String[] args) {
         int a=20;
         System.out.println(--a + a++ + a--);//19+19+20
-        System.out.println(a);
+        System.out.println(a);//19
     }
 }
